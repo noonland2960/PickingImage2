d6986e33e2dada4321ac21432e3eaee508c84a88
shogo355 <shogo355@hotmail.com>
2021-05-01 07:59:18 +0900

    Initial Commit

---
+++ PickingImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PickingImage
+//
+//  Created by 嶋田　省吾 on 2021/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
